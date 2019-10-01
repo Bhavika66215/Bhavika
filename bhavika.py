62c8905a729b8df2f5d50f3da81c4c3a15854b47
Bhavika66215 <56036668+Bhavika66215@users.noreply.github.com>
2019-10-01 20:32:47 +0530

    Update and rename README.md to bhavika.py
    This Program is to find greater number between two given numbers

---
+++ bhavika.py
@@ -0,0 +1,7 @@
+# Bhavika
+a=int(input("Enter the first number:",end=" "))
+b=int(input("Enter the second number:",end=" "))
+if (a>b):
+    print(a,"is greater than",b)
+else:
+    print(b,"is greater than",a)
